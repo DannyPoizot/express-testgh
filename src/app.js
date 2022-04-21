349b3d0c347f82993d1af5578459b70659358c1e
DannyPoizot <poizot.dan@gmail.com>
2022-04-21 12:34:08 +0200

    update port

--- src/app.js
+++ src/app.js
@@ -22,4 +22,4 @@ app.get('/api/dashboard', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Running on Port 3000'));
+app.listen(5000, () => console.log('Running on Port 5000'));
